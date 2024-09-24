cbd24e30c06ac37f9e2f607dcb7170aeff1d4a48
MarlonChi <marlonchiodelli@hotmail.com>
2024-09-24 13:35:36 -0300

    navbar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,49 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { CiMenuFries } from "react-icons/ci";
+
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { links } from "@/utils/nav-links";
+
 const MobileNav = () => {
-  return <nav>MobileNav</nav>;
+  const pathname = usePathname();
+
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Marlon <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } capitalize font-medium hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
