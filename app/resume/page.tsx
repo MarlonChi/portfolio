2d6d41647a7e15167259724a03816e7e8cb5ef93
MarlonChi <marlonchiodelli@hotmail.com>
2024-09-28 18:18:39 -0300

    feat: resume page structure

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,193 @@
-import React from "react";
+"use client";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { motion } from "framer-motion";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+const about = {
+  title: "About me",
+  description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Marlon Chiodelli",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+55 54 99991 9525",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "5+ Years",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Brasilian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "marlonchiodelli@hotmail.com",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Portuguese",
+    },
+  ],
+};
+
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  items: [
+    {
+      company: "Tech Innovation Solutions",
+      position: "Software Engineer",
+      duration: "2022 - Present",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, quos alias.",
+    },
+    {
+      company: "Compass.uol",
+      position: "Front-end Developer",
+      duration: "2019 - Present",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, quos alias.",
+    },
+  ],
+};
+
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My education",
+  description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  items: [
+    {
+      institution: "Online Course Platform",
+      degree: "Full Stack Web Development Bootcamp",
+      duration: "2022",
+    },
+    {
+      institution: "Code Academy",
+      degree: "Front-end Track",
+      duration: "2020 - 2023",
+    },
+  ],
+};
+
+const skills = {
+  title: "My skills",
+  description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "html 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <FaReact />,
+      name: "react.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwind.css",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "node.js",
+    },
+    {
+      icon: <FaFigma />,
+      name: "figma",
+    },
+  ],
+};
 
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 2.4,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul>
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <span>{item.duration}</span>
+                          <h3>{item.position}</h3>
+                          <div>
+                            <span></span>
+                            <p>{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              education
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              about me
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
