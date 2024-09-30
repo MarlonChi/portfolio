6c4a0a5cb83eaa8f0b0a7cb4ac59e735973e57e7
MarlonChi <marlonchiodelli@hotmail.com>
2024-09-29 21:16:16 -0300

    feat: photo animation

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -30,14 +30,14 @@ const Photo = () => {
         </motion.div>
 
         <motion.svg
-          className="w-[300px] xl:w-[506px] h-[300px] xl:h[506px]"
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
           fill="transparent"
           viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
         >
           <motion.circle
-            cx="253"
-            cy="253"
+            cx="252"
+            cy="252"
             r="250"
             stroke="#00ff99"
             strokeWidth="4"
